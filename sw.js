64059ae4f6e6fc5dce1dcecf6799515500b00cdd
kioscoa220munro <kiosco.a220@gmail.com>
2026-08-31 21:04:48 -0300

    Éter v0.8: renovar caché móvil

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='eter-v0.7';
+const CACHE='eter-v0.8';
 const SHELL=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('eter-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
